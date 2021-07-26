eae0b8660130701f20c49cd3f24bd68769b43334
aleksandrVorobey <69941253+aleksandrVorobey@users.noreply.github.com>
2021-07-26 16:41:52 +0300

    add showHideButton

--- organaizer/organaizer/Controllers/ScheduleViewController.swift
+++ organaizer/organaizer/Controllers/ScheduleViewController.swift
@@ -15,29 +15,75 @@ class ScheduleViewController: UIViewController {
         calendar.translatesAutoresizingMaskIntoConstraints = false
         return calendar
     }()
+    
+    var calendarHeightConstrain: NSLayoutConstraint!
+    
+    let showHideButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Open calendar", for: .normal)
+        button.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir Next Demi Bold", size: 16)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Schedule"
         view.backgroundColor = .white
-        setConstraints()
+        setConstraintsForCalendar()
+        
+        calendar.delegate = self
+        calendar.dataSource = self
+        
+        calendar.scope = .week
+        
+        showHideButton.addTarget(self, action: #selector(showHideButtonTapped), for: .touchUpInside)
     }
     
+    @objc func showHideButtonTapped() {
+        if calendar.scope == .week {
+            calendar.setScope(.month, animated: true)
+            showHideButton.setTitle("Close calendar", for: .normal)
+        } else {
+            calendar.setScope(.week, animated: true)
+            showHideButton.setTitle("Open calendar", for: .normal)
+        }
+    }
 
+}
+
+//MARK: - FSCalendar delegate and datasourse
+extension ScheduleViewController: FSCalendarDelegate, FSCalendarDataSource {
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeightConstrain.constant = bounds.height
+        view.layoutIfNeeded()
+    }
     
-
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
 }
 
 // MARK: - SetConstraints
 extension ScheduleViewController {
-    func setConstraints() {
+    func setConstraintsForCalendar() {
         view.addSubview(calendar)
+        view.addSubview(showHideButton)
+        
+        calendarHeightConstrain = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        calendar.addConstraint(calendarHeightConstrain)
+        
         NSLayoutConstraint.activate([
             calendar.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
             calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
             calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            calendar.heightAnchor.constraint(equalToConstant: 300)
+            
+            showHideButton.topAnchor.constraint(equalTo: calendar.bottomAnchor, constant: 0),
+            showHideButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            showHideButton.widthAnchor.constraint(equalToConstant: 120),
+            showHideButton.heightAnchor.constraint(equalToConstant: 20)
         ])
     }
 }
