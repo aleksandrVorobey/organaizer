5b96bb97718577917e54a8790aa505372b125a10
aleksandrVorobey <69941253+aleksandrVorobey@users.noreply.github.com>
2021-08-02 15:07:07 +0300

    add SceduleColorViewCell

--- organaizer/organaizer/Controllers/SheduleViewController/ScheduleColorViewController.swift
+++ organaizer/organaizer/Controllers/SheduleViewController/ScheduleColorViewController.swift
@@ -7,14 +7,64 @@
 
 import UIKit
 
-class ScheduleColorViewController: UIViewController {
-
+class ScheduleColorViewController: UITableViewController {
+    
+    let idOptionsColorCell = "idOptionsColorCell"
+    let idOptionsColorHeader = "idOptionsColorHeader"
+    
+    let headerNameArray = ["RED", "ORANGE", "YELLOW", "GREEN", "BLUE", "DEEP BLUE", "PURPLE"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        title = "Colors"
-        view.backgroundColor = .white
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(ColorScheduleTableViewCell.self, forCellReuseIdentifier: idOptionsColorCell)
+        tableView.register(HeaderOptionsTableViewCell.self, forHeaderFooterViewReuseIdentifier: idOptionsColorHeader)
+        
+        tableView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        tableView.separatorStyle = .none
+        tableView.bounces = false
+        
+        title = "Color Schedule"
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 7
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: idOptionsColorCell, for: indexPath) as! ColorScheduleTableViewCell
+        cell.cellConfigure(indexPath: indexPath)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! ColorScheduleTableViewCell
+        
+        print("Tap cell")
+    }
+    
+    
+//MARK: - Create Header
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: idOptionsColorHeader) as! HeaderOptionsTableViewCell
+        header.headerConfigure(nameArray: headerNameArray, section: section)
+        return header
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
     }
     
 }
+
 
